149758afdd5c970ea589840248397501315c3988
tjerk.valentijn <tjerk.valentijn@ebpi.nl>
2018-12-13 16:07:41 +0100

    minimally working version of request-response with async await

--- session-1/answer-4/request-response/src/form/FormComponent.js
+++ session-1/answer-4/request-response/src/form/FormComponent.js
@@ -1,30 +1,39 @@
 import React from "react";
 
 /**
- * TODO use the props to bind and evoke parent handler
+ * Functional presentational component, is like a template and just renders and nothing more
+ * props are used to bind events to it's parents handler
  */
-const FormComponent = ({ formData, saveHandler }) => {
+const FormComponent = ({
+  planet,
+  allegiance,
+  handlePlanetChange,
+  handleAllegianceChange,
+  handleSaveButton
+}) => {
   return (
     <section>
       <form>
-        <label>
-          Planet:{" "}
-          <input
-            type="text"
-            value={this.state.planet}
-            onChange={this.handlePlanetChange}
-          />
-        </label>
-
-        <label>
-          Allegiance:
-          <div className="input-field">
-            <select onChange={handleSelect}>
-              <option label="rebels">rebels</option>
-              <option label="empire">empire</option>
-            </select>
-          </div>
-        </label>
+        <div className="form-group">
+          <label>
+            Planet:
+            <input type="text" value={planet} onChange={handlePlanetChange} />
+          </label>
+        </div>
+        <div className="form-group">
+          <label>
+            <div className="input-field">
+              Allegiance:
+              <select onChange={handleAllegianceChange}>
+                <option label="rebels">rebels</option>
+                <option label="empire">empire</option>
+              </select>
+            </div>
+          </label>
+        </div>
+        <div className="form-group">
+          <button onClick={handleSaveButton}>save</button>
+        </div>
       </form>
     </section>
   );
--- session-1/answer-4/request-response/src/form/FormContainer.js
+++ session-1/answer-4/request-response/src/form/FormContainer.js
@@ -1,9 +1,11 @@
 import React from "react";
 import FormComponent from "./FormComponent";
 
+const SAVE_PLANET_URL = "planets/";
+
 export default class FormContainer extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       planet: "",
       allegiance: ""
@@ -11,14 +13,47 @@ export default class FormContainer extends React.Component {
   }
 
   /**
-   * Eventhandler is pulled up into a function.
+   * Event handler is pulled up into a function.
    * Remove arrow notation and watch it fail
    */
   handlePlanetChange = evt => {
     this.setState({ planet: evt.target.value });
   };
 
+  handleAllegianceChange = evt => {
+    this.setState({ allegiance: evt.target.value });
+  };
+
+  /**
+   * cannot use async and phat arrow in one go :-(
+   */
+  async handleSaveButton(evt) {
+    evt.preventDefault();
+    console.log("handleSaveButton");
+    try {
+      // note that fetch will handle a non 200 (ok) response not as an exception
+      const response = await fetch(SAVE_PLANET_URL, {
+        method: "POST",
+        body: this.state
+      });
+      // do something with response, see F12 for a dump of the response object
+      console.dir(response);
+      alert(`${response.status}: ${response.statusText}`);
+    } catch (error) {
+      // error could be a network error
+      alert(error);
+    }
+  }
+
   render() {
-    return <FormComponent />;
+    return (
+      <FormComponent
+        planet={this.state.planet}
+        allegiance={this.state.allegiance}
+        handlePlanetChange={this.handlePlanetChange}
+        handleAllegianceChange={this.handleAllegianceChange}
+        handleSaveButton={this.handleSaveButton.bind(this)} // manually binding 'this' for the method
+      />
+    );
   }
 }
